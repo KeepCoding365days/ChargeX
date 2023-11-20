6c8bc5c681ef4b382324274dd1d35ad4912c100c
Muhammad Affan <azharaffan45@gmail.com>
2023-11-20 23:36:20 +0500

    signUp added

--- app/src/main/java/com/example/chargex/SignUp.java
+++ app/src/main/java/com/example/chargex/SignUp.java
@@ -1,20 +1,12 @@
 package com.example.chargex;
 
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.chargex.databinding.ActivitySignUpBinding;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -22,39 +14,23 @@ import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivitySignUpBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+    public void SignUp(View v){
+        TextView email_view=findViewById(R.id.SignUpEmailAddress);
+        String email=email_view.getText().toString();
+        TextView pw_view=findViewById(R.id.SignUpPassword);
+        String pw=pw_view.getText().toString();
         FirebaseFirestore db=FirebaseFirestore.getInstance();
         Map<String,Object> user= new HashMap<>();
-        user.put("name","Numair");
-        user.put("email","numair@gmail.com");
-        db.collection("Person").add(user);
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_sign_up);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
+        user.put("email",email);
+        user.put("password",pw);
+        DocumentReference doc= db.collection("Person").document(email);
+        doc.set(user);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_sign_up);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
